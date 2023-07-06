e6aa00d4977a872afd7af9309241dd589c25545a
Егор <egordavydov95@gmail.com>
2023-07-06 19:00:39 +0300

    settings

--- spectech/spectech/settings.py
+++ spectech/spectech/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tracker'
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,14 @@ WSGI_APPLICATION = 'spectech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spectech',
+        'USER': 'postgres',
+        'PASSWORD': 'qwe',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
-}
+ }
 
 
 # Password validation
