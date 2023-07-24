4815d4e4de84a1923dce5d4c15e9426204a7e468
gipopotamus <108004878+gipopotamus@users.noreply.github.com>
2023-07-24 15:31:04 +0300

    Update settings.py

--- spectech/spectech/settings.py
+++ spectech/spectech/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [env("ALLOWED_HOST"), ]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
