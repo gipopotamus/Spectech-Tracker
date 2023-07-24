313c40923e9527036f9caf4a4412ab551284fd21
gipopotamus <108004878+gipopotamus@users.noreply.github.com>
2023-07-24 17:40:19 +0300

    Update settings.py

--- spectech/spectech/settings.py
+++ spectech/spectech/settings.py
@@ -25,7 +25,7 @@ load_dotenv('./.env')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
