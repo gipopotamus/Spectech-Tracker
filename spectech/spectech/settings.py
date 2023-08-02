8475cf6b9eb001addf00592f1bed9fb06b1640fc
gipopotamus <108004878+gipopotamus@users.noreply.github.com>
2023-08-03 00:31:00 +0300

    media

--- spectech/spectech/settings.py
+++ spectech/spectech/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
